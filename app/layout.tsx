da1774559c0b58b74326fe3087dc600fd322498d
soneryesilay <soneryesilay@outlook.com>
2025-04-17 22:32:42 +0300

    vercel analtics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { ThemeProvider } from '../components/theme-provider'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'Soner Yeşilay Portfolio',
@@ -33,6 +34,7 @@ export default function RootLayout({
         >
           {children}
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
